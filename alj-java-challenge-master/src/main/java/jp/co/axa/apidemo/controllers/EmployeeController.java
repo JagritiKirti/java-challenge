934d6c88d138751f878c65aaaa88bc9b91f6bd8b
JagritiKirti <jagriti.kirti@gmail.com>
2022-02-14 15:13:39 +0900

    Loggers added

--- alj-java-challenge-master/src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
+++ alj-java-challenge-master/src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
@@ -3,6 +3,9 @@ package jp.co.axa.apidemo.controllers;
 import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.model.EmployeeDetails;
 import jp.co.axa.apidemo.services.EmployeeService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,8 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
 
     @Autowired
     private EmployeeService employeeService;
@@ -26,6 +31,7 @@ public class EmployeeController {
     @ApiOperation(value = "This method is used to fetch the list of employees")
     @GetMapping("/employees")
     public ResponseEntity<List<EmployeeDetails>> getEmployees() {
+    	LOG.info("Inside EmployeeController - getEmployees");
         List<Employee> employees = employeeService.retrieveEmployees();
         List<EmployeeDetails> employeesResponse = employees.stream().map(
         		e -> new EmployeeDetails(e)).collect(Collectors.toList());
@@ -35,6 +41,7 @@ public class EmployeeController {
     @ApiOperation(value = "This method is used to fetch the employee data for given employee ID")
     @GetMapping("/employees/{employeeId}")
     public ResponseEntity<EmployeeDetails> getEmployee(@PathVariable(name="employeeId")Long employeeId) {    	
+    	LOG.info("Inside EmployeeController - getEmployee -- employeeId {}", employeeId);
     	EmployeeDetails employee = new EmployeeDetails(employeeService.getEmployee(employeeId));
         return ResponseEntity.ok().body(employee);      
     }
@@ -42,7 +49,7 @@ public class EmployeeController {
     @ApiOperation(value = "This method is used to save employee record")
     @PostMapping("/employees")
     public ResponseEntity<String> saveEmployee(@RequestBody EmployeeDetails employeeDetails){
-    	
+    	LOG.info("Inside EmployeeController - saveEmployee");
         employeeService.saveEmployee(new Employee(employeeDetails));
         return ResponseEntity.ok().body("Employee Saved Successfully");
     }
@@ -50,7 +57,8 @@ public class EmployeeController {
     @ApiOperation(value = "This method is used to delete the employee record based on employee ID")
     @DeleteMapping("/employees/{employeeId}")
     public ResponseEntity<String>  deleteEmployee(@PathVariable(name="employeeId")Long employeeId){
-        employeeService.deleteEmployee(employeeId);
+    	LOG.info("Inside EmployeeController - deleteEmployee -- employeeId {}", employeeId);
+    	employeeService.deleteEmployee(employeeId);
         return ResponseEntity.ok().body("Employee Deleted Successfully");
     }
 
@@ -58,6 +66,7 @@ public class EmployeeController {
     @PutMapping("/employees/{employeeId}")
     public ResponseEntity<String> updateEmployee(@RequestBody EmployeeDetails employeeDetails,
                                @PathVariable(name="employeeId")Long employeeId){
+    	LOG.info("Inside EmployeeController - updateEmployee -- employeeId {}", employeeId);
         Employee emp = employeeService.getEmployee(employeeId);
         if(emp != null){
             employeeService.updateEmployee(new Employee(employeeDetails));
--- alj-java-challenge-master/src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
+++ alj-java-challenge-master/src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
@@ -2,6 +2,9 @@ package jp.co.axa.apidemo.services;
 
 import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.repositories.EmployeeRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
@@ -13,6 +16,8 @@ import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
+	
+	private static final Logger LOG = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -22,28 +27,33 @@ public class EmployeeServiceImpl implements EmployeeService{
     }
 
     public List<Employee> retrieveEmployees() {
+    	LOG.info("Inside EmployeeServiceImpl - retrieveEmployees");
         List<Employee> employees = employeeRepository.findAll();
         return employees;
     }
 
     @Cacheable(cacheNames = "employeeCache" ,key = "#id")
     public Employee getEmployee(Long id) {
+    	LOG.info("Inside EmployeeServiceImpl - getEmployee -- employeeId {} ", id);
         Optional<Employee> optEmp = employeeRepository.findById(id);
         return optEmp.get();
     }
 
     public void saveEmployee(Employee employee){
+    	LOG.info("Inside EmployeeServiceImpl - saveEmployee");
         employeeRepository.save(employee);
     }
 
     @CacheEvict(cacheNames = "employeeCache" ,key = "#id")
     public void deleteEmployee(Long id){
+    	LOG.info("Inside EmployeeServiceImpl - deleteEmployee -- employeeId {}", id);
         employeeRepository.deleteById(id);
     }
 
     @CacheEvict(cacheNames = "employeeCache" ,key = "#employee.id")
     @CachePut(cacheNames = "employeeCache" ,key = "#employee.id")
     public void updateEmployee(Employee employee) {
+    	LOG.info("Inside EmployeeServiceImpl - saveEmployee");
         employeeRepository.save(employee);
     }
 }
